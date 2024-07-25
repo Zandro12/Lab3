dc20513db137813df9f9efab9307635fbd3ed471
Derrif2005 <Ugerardin@gmail.com>
2024-07-24 19:03:57 -0600

    a
    a

--- gestor_estudiantes.py
+++ gestor_estudiantes.py
@@ -1,8 +1,19 @@
-Estudiante=[]
+estudiantes=[]
 def Agregar_estudiante(carnet,nombre,edad,grado,calificaciones=[]):
-    Estudiante=[
-        "carnet":carnet,
-        "nombre":nombre,
-    ]
+    estudiante={
+        "carnet": carnet,
+        "nombre": nombre,
+        "edad": edad,
+        "grado":grado,
+    "calificaciones": calificaciones
+    }
+    estudiantes.append(estudiante)
+def buscar_estudiante(carnet):
+    for estudiante in estudiantes:
+        if estudiante["carnet"] == carnet:
+            return
+def modificar_estudiante(carnet,nombre,edad,grado,calificaciones):
+    estudiante = buscar_estudiante(carnet)
+    if estudiante
 
 
