084454d4cb21c49d4bdedb9d11f60269732b82e9
Derrif2005 <Ugerardin@gmail.com>
2024-07-24 21:14:52 -0600

    cambio
    a

--- gestor_estudiantes.py
+++ gestor_estudiantes.py
@@ -1,58 +1,58 @@
-estudiantes = []
+estudiantes=[]
 
-def agregar_estudiante(carnet, nombre, edad, calificaciones):
-    estudiante = {
-        "carnet": carnet,
-        "nombre": nombre,
-        "edad": edad,
-        "calificaciones": []
+def agregar_estudiante(carnet, nombre, edad):
+    estudiante={
+        "carnet":carnet,
+        "nombre":nombre,
+        "edad":edad,
+        "calificaciones":[]
     }
     estudiantes.append(estudiante)
 
 def buscar_estudiante(carnet):
     for estudiante in estudiantes:
-        if estudiante["carnet"] == carnet:
+        if estudiante["carnet"]==carnet:
             return estudiante
     return None
 
 def modificar_estudiante(carnet, nombre, edad, calificaciones):
-    estudiante = buscar_estudiante(carnet)
+    estudiante=buscar_estudiante(carnet)
     if estudiante:
-        estudiante["nombre"] = nombre
-        estudiante["edad"] = edad
-        estudiante["calificaciones"] = calificaciones
+        estudiante["nombre"]=nombre
+        estudiante["edad"]=edad
+        estudiante["calificaciones"]=calificaciones
         return True
     return False
 
 def eliminar_estudiante(carnet):
-    estudiante = buscar_estudiante(carnet)
+    estudiante=buscar_estudiante(carnet)
     if estudiante:
         estudiantes.remove(estudiante)
         return True
     return False
 
 def calificar_estudiante(carnet, calificaciones):
-    estudiante = buscar_estudiante(carnet)
+    estudiante=buscar_estudiante(carnet)
     if estudiante:
         for calificacion in calificaciones:
-            materia, nota = calificacion
-            estudiante["calificaciones"].append({"materia": materia, "nota": nota})
+            materia, nota=calificacion
+            estudiante["calificaciones"].append({"materia":materia, "nota":nota})
         return True
     return False
 
 def promedio_calificaciones(carnet):
-    estudiante = buscar_estudiante(carnet)
+    estudiante=buscar_estudiante(carnet)
     if estudiante and estudiante["calificaciones"]:
-        total = sum(calificacion["nota"] for calificacion in estudiante["calificaciones"])
+        total=sum(calificacion["nota"] for calificacion in estudiante["calificaciones"])
         return total / len(estudiante["calificaciones"])
     return 0
 
 def lista_aprobados_reprobados():
-    aprobados = []
-    reprobados = []
+    aprobados=[]
+    reprobados=[]
     for estudiante in estudiantes:
-        promedio = promedio_calificaciones(estudiante["carnet"])
-        if promedio >= 7:
+        promedio=promedio_calificaciones(estudiante["carnet"])
+        if promedio>=7:
             aprobados.append(estudiante)
         else:
             reprobados.append(estudiante)
